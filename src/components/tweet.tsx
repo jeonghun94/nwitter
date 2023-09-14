f81d4d3221d90f6bc475e14482814a43a42971ea
조정훈 <jeongh1021@naver.com>
2023-09-14 18:05:37 +0900

    트윗 생성, 삭제 기능 및 프로필 업데이트 추가

---
+++ src/components/tweet.tsx
@@ -0,0 +1,94 @@
+import { styled } from "styled-components";
+import { ITweet } from "./timeline";
+import { auth, db, storage } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  padding: 20px;
+  border: 1px solid rgba(255, 255, 255, 0.5);
+  border-radius: 15px;
+`;
+
+const Column = styled.div`
+  &:last-child {
+    place-self: end;
+  }
+`;
+
+const Photo = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 15px;
+`;
+
+const Username = styled.span`
+  font-weight: 600;
+  font-size: 15px;
+`;
+
+const Payload = styled.p`
+  margin: 10px 0px;
+  font-size: 18px;
+`;
+
+const Button = styled.button`
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const DeleteButton = styled(Button)`
+  background-color: tomato;
+`;
+
+const UpdateButton = styled(Button)`
+  background-color: #1d9bf0;
+`;
+
+export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
+  const user = auth.currentUser;
+  const onDelete = async () => {
+    const ok = confirm("Are you sure you want to delete this tweet?");
+    if (!ok || user?.uid !== userId) return;
+    try {
+      await deleteDoc(doc(db, "tweets", id));
+      if (photo) {
+        const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
+        await deleteObject(photoRef);
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      //
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Column>
+        <Username>{username}</Username>
+        <Payload>{tweet}</Payload>
+        {user?.uid === userId ? (
+          <ButtonWrapper>
+            <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+            <UpdateButton>Update</UpdateButton>
+          </ButtonWrapper>
+        ) : null}
+      </Column>
+      <Column>{photo ? <Photo src={photo} /> : null}</Column>
+    </Wrapper>
+  );
+}
